28abd255cc43e4f7b3e71db4aa3b2360a26c2dfb
Lars Brinkhoff <lars@nocrew.org>
2015-11-27 09:17:53 +0100

    Refactor install script.

--- deps.sh
+++ deps.sh
@@ -1,36 +1,80 @@
-install_linux() {
+prepare_linux() {
   sudo apt-get update -yqq
-  sudo apt-get install -qy ${FORTH:-gforth}
+  install=install_apt
+  libs32="libc6-i386"
+}
+
+prepare_yum() {
+  install=install_yum
+}
+
+prepare_rpm() {
+  install=install_rpm
+}
+
+prepare_osx() {
+  brew update > /dev/null
+  install=install_brew
+}
+
+prepare_gforth() {
+  true
+}
+
+prepare_gforth_rpm() {
+  sudo yum install libtool-ltdl-devel
+  forth='http://download.opensuse.org/repositories/home:/forthy/CentOS_CentOS-6/x86_64/gforth-0.7.9_20131227-63.1.x86_64.rpm'
+}
+
+prepare_pforth() {
+  true
+}
+
+prepare_vfxforth() {
+  $install $libs32 wget
+  install=install_download
+  forth='ftp://public@soton.mpeforth.com/VfxLinEval/vfxforth-eval-4.72-0540.i386.tar.gz'
+}
+
+prepare_spforth() {
+  $install $libs32 wget
+  install=install_download
+  forth='http://downloads.sourceforge.net/spf/spf-4.20.tar.gz'
+}
+
+prepare_lina() {
+  $install wget
+  install=install_download
+  forth='http://home.hccnet.nl/a.w.m.van.der.horst/lina64-5.2.1.tar.gz'
+}
+
+install_apt() {
+  sudo apt-get install -qy "$@"
+}
+
+install_rpm() {
+  wget -q "$1"
+  sudo rpm -i --force *.rpm
 }
 
 install_yum() {
-   wget http://download.opensuse.org/repositories/home:/forthy/CentOS_CentOS-6/x86_64/gforth-0.7.9_20131227-63.1.x86_64.rpm
-   sudo yum install libtool-ltdl-devel
-   sudo rpm -i --force gforth*.rpm
+  sudo yum install "$@"
 }
 
 install_osx() {
-  brew update > /dev/null
-  brew install ${FORTH:-gforth}
+  brew install "$@"
 }
 
-download_spforth() {
-  sudo apt-get update -yqq
-  sudo apt-get install -qy lib32gcc1 libc6-i386 wget
-  wget -q http://downloads.sourceforge.net/spf/spf-4.20.tar.gz
-  tar xzf spf*.tar.gz
+install_download() {
+  wget -q "$1" -O- | tar xzf -
 }
 
-download_lina() {
-  wget http://home.hccnet.nl/a.w.m.van.der.horst/lina64-5.2.1.tar.gz
-  tar xzf lina64*.tar.gz
+maybe() {
+ "$1" || true
 }
 
-download_vfxforth() {
-  sudo apt-get update -yqq
-  sudo apt-get install -qy lib32gcc1 libc6-i386 wget
-  wget -q ftp://public@soton.mpeforth.com/VfxLinEval/vfxforth-eval-4.72-0540.i386.tar.gz
-  tar xzf vfxforth*.tar.gz
-}
-
-$1
+forth="$2"
+prepare_$1
+prepare_$2
+maybe prepare_$2_$1
+$install "$forth"
